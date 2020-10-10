b55c15774406c0a7c916a0c5f7186f05e22136a5
JorgeEmilioRomero <62726140+JorgeEmilioRomero@users.noreply.github.com>
2020-10-10 00:25:19 -0500

    Primer commit

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$connectstr_dbhost = 'serverhuellas1.mysql.database.azure.com';
-$connectstr_dbusername = 'huellasadmin@serverhuellas1';
-$connectstr_dbpassword = 'comovamos84.';
-$connectstr_dbname = 'localdb';
-
-$sen = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
